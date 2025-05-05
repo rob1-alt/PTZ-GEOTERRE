a0632729fc92b02783b5c82b7c78623e96d1bf9d
rob1alt <robin.pautigny@hetic.net>
2025-05-05 11:17:34 +0200

    embed image

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,32 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'PTZ Simulateur Geoterre',
-  description: 'Geoterre',
+  description: 'Simulateur pour calculer votre éligibilité au Prêt à Taux Zéro 2025',
   icons: {
     icon: '/favicon.png',
     apple: '/favicon.png'
+  },
+  openGraph: {
+    title: 'PTZ Simulateur Geoterre',
+    description: 'Simulateur pour calculer votre éligibilité au Prêt à Taux Zéro 2025',
+    url: 'https://simulateur-ptz-2025.fr',
+    siteName: 'PTZ Simulateur Geoterre',
+    images: [
+      {
+        url: '/image.png',
+        width: 1200,
+        height: 630,
+        alt: 'PTZ Simulateur Geoterre'
+      }
+    ],
+    locale: 'fr_FR',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'PTZ Simulateur Geoterre',
+    description: 'Simulateur pour calculer votre éligibilité au Prêt à Taux Zéro 2025',
+    images: ['/image.png'],
   }
 }
 
